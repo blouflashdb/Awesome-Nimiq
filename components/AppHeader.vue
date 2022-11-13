a235e471d8f4f6e31622a595b7b7509832300ef7
blouflashdb <45914736+blouflashdb@users.noreply.github.com>
2022-11-13 23:33:26 +0100

    feat: add i18n

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,14 @@
 <template>
   <header>
-    <h1>Header</h1>
+    <h1>{{ $t("hello", { name: "vue-i18n" }) }}</h1>
+    <form>
+      <label for="locale-select">{{ $t("language") }}: </label>
+      <select
+        id="locale-select"
+        v-model="$i18n.locale">
+        <option value="en">en</option>
+        <option value="de">de</option>
+      </select>
+    </form>
   </header>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 import svgLoader from "vite-svg-loader";
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "url";
+import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
@@ -13,6 +16,18 @@ export default defineNuxtConfig({
   css: ["@nimiq/style", "~/assets/fonts/fonts.scss"],
 
   vite: {
-    plugins: [svgLoader()],
+    resolve: {
+      alias: {
+        "vue-i18n": "vue-i18n/dist/vue-i18n.runtime.esm-bundler.js",
+      },
+    },
+    plugins: [
+      svgLoader(),
+      VueI18nVitePlugin({
+        include: [
+          resolve(dirname(fileURLToPath(import.meta.url)), "./locales/*.json"),
+        ],
+      }),
+    ],
   },
 });
